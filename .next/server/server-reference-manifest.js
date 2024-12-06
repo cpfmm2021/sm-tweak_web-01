d57e5ed94a2c3660d34917e00120556550a9772a
cpfmm2021 <cpfmmmr@gmail.com>
2024-12-06 19:48:38 +0900

    Config: Change development port to 3009

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Viw8K6M4AXZXArKWDJmED3N/dcnJBWgvaUsbByXSJCE=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
